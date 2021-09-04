29fec37548a948230439dc0938c7525d3a324a10
tks1998 <trankimsen1681998@gmail.com>
2021-09-04 23:00:28 +0700

    convert proxy list to tensor

--- code/losses.py
+++ code/losses.py
@@ -161,7 +161,7 @@ class ProxyAnchor_Newton(torch.nn.Module):
 
     def sim_matrix(a, b, eps=1e-8):
         """ added eps for numerical stability"""
-        a_n, b_n = a.norm(dim=1)[:, None], b.norm(dim=1)[:, None]
+        a_n, b_n = torch.norm(a,dim=1)[:, None], torch.norm(b,dim=1)[:, None]
         a_norm = a / torch.max(a_n, eps * torch.ones_like(a_n))
         b_norm = b / torch.max(b_n, eps * torch.ones_like(b_n))
         sim_mt = torch.mm(a_norm, b_norm.transpose(0, 1))
