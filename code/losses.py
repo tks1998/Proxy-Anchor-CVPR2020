4748e55573a8940b0947d66ff815625380cd2157
tks1998 <trankimsen1681998@gmail.com>
2021-09-04 22:56:24 +0700

    convert proxy list to tensor

--- code/losses.py
+++ code/losses.py
@@ -146,27 +146,21 @@ class AdaptiveProxyAnchorLossAutoscale(torch.nn.Module):
         loss = pos_term + neg_term - self.scale_margin*(torch.mean(self.mrg))*(torch.abs(pos_term-neg_term))
         return loss
 
+
 class ProxyAnchor_Newton(torch.nn.Module):
-    def __init__(self, nb_classes, sz_embed, alpha=32, nb_proxies=1,scale_margin=10):
+    def __init__(self, nb_classes, sz_embed, mrg=0.1, alpha=32):
         torch.nn.Module.__init__(self)
         # Proxy Anchor Initialization
+        self.proxies = torch.nn.Parameter(torch.randn(nb_classes, sz_embed).cuda())
+        nn.init.kaiming_normal_(self.proxies, mode='fan_out')
+
         self.nb_classes = nb_classes
         self.sz_embed = sz_embed
-        # self.mrg = torch.nn.Parameter(torch.tensor([mrg] * nb_classes, requires_grad=True, device='cuda',
-        #                                            dtype=torch.double))  # -> mrg -> list[...nb_classes]
+        self.mrg = mrg
         self.alpha = alpha
-        self.nb_proxies = nb_proxies
-        self.proxies_list = []
-        self.scale_margin = scale_margin
-        for i in range(self.nb_proxies):
-            self.proxies = torch.nn.Parameter(torch.randn(nb_classes, sz_embed).cuda())
-            nn.init.kaiming_normal_(self.proxies, mode='fan_out')
-            self.proxies_list.append(self.proxies)
-        self.proxies_list = torch.Tensor(self.proxies_list)
+
     def sim_matrix(a, b, eps=1e-8):
-        """
-        added eps for numerical stability
-        """
+        """ added eps for numerical stability"""
         a_n, b_n = a.norm(dim=1)[:, None], b.norm(dim=1)[:, None]
         a_norm = a / torch.max(a_n, eps * torch.ones_like(a_n))
         b_norm = b / torch.max(b_n, eps * torch.ones_like(b_n))
@@ -174,21 +168,17 @@ class ProxyAnchor_Newton(torch.nn.Module):
         return sim_mt
 
     def forward(self, X, T):
-        P = self.proxies_list
+        P = self.proxies
 
-        cos_list = [F.linear(l2_norm(X), l2_norm(P[i])) for i in range(self.nb_proxies)]
-        new_ton_cos_list = [F.linear(X, P[i])/self.sim_matrix(X,P[i]) for i in range(self.nb_proxies)]
-
-        tensor_sum_cos = torch.stack([i for i in cos_list])
-        margin = torch.stack([i for i in new_ton_cos_list])
-        _data_cos = torch.mean(tensor_sum_cos, dim=0)
-        margin_mean = torch.stack([i for i in new_ton_cos_list])
-        print(margin_mean)
+        cos = F.linear(l2_norm(X), l2_norm(P))  # Calcluate cosine similarity
+        new_ton_cos_list = torch.norm(X,dim=1)*torch.norm(P,dim=1).reshape(-1,1) / self.sim_matrix(X, P)
+        mean_new_ton = torch.mean(new_ton_cos_list, dim=1)
+        print(mean_new_ton)
         P_one_hot = binarize(T=T, nb_classes=self.nb_classes)
         N_one_hot = 1 - P_one_hot
 
-        pos_exp = torch.exp(-self.alpha * (_data_cos - margin_mean))
-        neg_exp = torch.exp(self.alpha * (_data_cos + margin_mean))
+        pos_exp = torch.exp(-self.alpha * (cos - mean_new_ton))
+        neg_exp = torch.exp(self.alpha * (cos + mean_new_ton))
 
         with_pos_proxies = torch.nonzero(P_one_hot.sum(dim=0) != 0).squeeze(
             dim=1)  # The set of positive proxies of data in the batch
@@ -199,9 +189,67 @@ class ProxyAnchor_Newton(torch.nn.Module):
 
         pos_term = torch.log(1 + P_sim_sum).sum() / num_valid_proxies
         neg_term = torch.log(1 + N_sim_sum).sum() / self.nb_classes
-
         loss = pos_term + neg_term
+
         return loss
+
+#
+# class ProxyAnchor_Newton(torch.nn.Module):
+#     def __init__(self, nb_classes, sz_embed, alpha=32, nb_proxies=1,scale_margin=10):
+#         torch.nn.Module.__init__(self)
+#         # Proxy Anchor Initialization
+#         self.nb_classes = nb_classes
+#         self.sz_embed = sz_embed
+#         # self.mrg = torch.nn.Parameter(torch.tensor([mrg] * nb_classes, requires_grad=True, device='cuda',
+#         #                                            dtype=torch.double))  # -> mrg -> list[...nb_classes]
+#         self.alpha = alpha
+#         self.nb_proxies = nb_proxies
+#         self.proxies_list = []
+#         self.scale_margin = scale_margin
+#         for i in range(self.nb_proxies):
+#             self.proxies = torch.nn.Parameter(torch.randn(nb_classes, sz_embed).cuda())
+#             nn.init.kaiming_normal_(self.proxies, mode='fan_out')
+#             self.proxies_list.(self.proxies)
+#         self.proxies_list = torch.Tensor(self.proxies_list)
+#     def sim_matrix(a, b, eps=1e-8):
+#         """
+#         added eps for numerical stability
+#         """
+#         a_n, b_n = a.norm(dim=1)[:, None], b.norm(dim=1)[:, None]
+#         a_norm = a / torch.max(a_n, eps * torch.ones_like(a_n))
+#         b_norm = b / torch.max(b_n, eps * torch.ones_like(b_n))
+#         sim_mt = torch.mm(a_norm, b_norm.transpose(0, 1))
+#         return sim_mt
+#
+#     def forward(self, X, T):
+#         P = self.proxies_list
+#
+#         cos_list = [F.linear(l2_norm(X), l2_norm(P[i])) for i in range(self.nb_proxies)]
+#         new_ton_cos_list = [F.linear(X, P[i])/self.sim_matrix(X,P[i]) for i in range(self.nb_proxies)]
+#
+#         tensor_sum_cos = torch.stack([i for i in cos_list])
+#         margin = torch.stack([i for i in new_ton_cos_list])
+#         _data_cos = torch.mean(tensor_sum_cos, dim=0)
+#         margin_mean = torch.stack([i for i in new_ton_cos_list])
+#         print(margin_mean)
+#         P_one_hot = binarize(T=T, nb_classes=self.nb_classes)
+#         N_one_hot = 1 - P_one_hot
+#
+#         pos_exp = torch.exp(-self.alpha * (_data_cos - margin_mean))
+#         neg_exp = torch.exp(self.alpha * (_data_cos + margin_mean))
+#
+#         with_pos_proxies = torch.nonzero(P_one_hot.sum(dim=0) != 0).squeeze(
+#             dim=1)  # The set of positive proxies of data in the batch
+#         num_valid_proxies = len(with_pos_proxies)  # The number of positive proxies
+#
+#         P_sim_sum = torch.where(P_one_hot == 1, pos_exp, torch.zeros_like(pos_exp)).sum(dim=0)
+#         N_sim_sum = torch.where(N_one_hot == 1, neg_exp, torch.zeros_like(neg_exp)).sum(dim=0)
+#
+#         pos_term = torch.log(1 + P_sim_sum).sum() / num_valid_proxies
+#         neg_term = torch.log(1 + N_sim_sum).sum() / self.nb_classes
+#
+#         loss = pos_term + neg_term
+#         return loss
 
 # We use PyTorch Metric Learning library for the following codes.
 # Please refer to "https://github.com/KevinMusgrave/pytorch-metric-learning" for details.
