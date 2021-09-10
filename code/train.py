674951dcabee0fb3d5c71de8bb23c948b7793360
tks1998 <trankimsen1681998@gmail.com>
2021-09-10 15:58:34 +0700

    record m

--- code/train.py
+++ code/train.py
@@ -373,4 +373,7 @@ for epoch in range(0, args.nb_epochs):
                 else:
                     for i in range(4):
                         f.write("Best Recall@{}: {:.4f}\n".format(10**i, best_recall[i] * 100))
+
+print("print ecord_margin")
+
 df_margin.to_csv("record_margin.csv")
